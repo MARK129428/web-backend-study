785afc1e08da497a93be5b6883cc2a2d250351bd
何鸿凯 <1544832671@qq.com>
2023-08-22 00:40:48 +0800

    项目初步串通

---
+++ hanlinxuefu/src/main/java/com/hhk/hanlinxuefu/HanlinxuefuApplication.java
@@ -0,0 +1,13 @@
+package com.hhk.hanlinxuefu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanlinxuefuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanlinxuefuApplication.class, args);
+    }
+
+}
---
+++ hanlinxuefu/src/test/java/com/hhk/hanlinxuefu/HanlinxuefuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhk.hanlinxuefu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanlinxuefuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
